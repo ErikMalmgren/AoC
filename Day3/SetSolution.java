0034af82868c0fbdbbdbbe76f24de1a2ee4b1011
Erik Malmgren <er8540ma-s@student.lu.se>
2022-12-03 09:36:02 +0100

    Bättre lösning

--- Day3/SetSolution.java
+++ Day3/SetSolution.java
@@ -1,17 +1,18 @@
 package Day3;
-
 import java.io.*;
+import java.util.*;
 
-public class Solution {
+
+public class SetSolution {
+    
     public static void main(String[] args) throws IOException {
         long startTime = System.nanoTime();
         System.out.println("Part one: " + partOne());
         System.out.println("Part two: " + partTwo());
         long elapsedTime = System.nanoTime() - startTime;
         System.out.println("Runtime in ms: " + elapsedTime/1000000);
-        
     }
-
+    // Ungefär dubbelt så snabb som första lösningen
 
     private static int partTwo() throws IOException {
         var input = new File("inputs/day3.txt");
@@ -20,33 +21,32 @@ public class Solution {
         String currentLine;
 
         while((currentLine = br.readLine()) != null) {
+            int biggest = 0; 
+
+            Set<Character> inString1 = new HashSet<>();
+            Set<Character> inString2 = new HashSet<>();
+
             String s1 = currentLine;
             String s2 = br.readLine();
             String s3 = br.readLine();
-            int biggest = 0;
 
-            for(int i = 0; i< s1.length(); i++) {
-                char c = s1.charAt(i);
-                int prio = Alphabet.getNum(c);
+            var c1 = s1.toCharArray();
+            var c2 = s2.toCharArray();
+            var c3 = s3.toCharArray();
 
-                for(int j = 0; j < s2.length(); j++) {
-                    if(c == s2.charAt(j)){
-                        for(int k = 0; k < s3.length(); k++) {
-                            if(c == s3.charAt(k)){
-                                if(prio > biggest) {
-                                    biggest = prio;
-                                }
-                            }
-                        }
-                    }
+            for(char c : c1) {
+                inString1.add(c);
+            }
+            for(char c : c2) {
+                inString2.add(c);
+            }
+            for(char c : c3) {
+                if(inString1.contains(c) && inString2.contains(c)) {
+                    biggest = Math.max(Alphabet.getNum(c), biggest);
                 }
             }
-
             sumPriority += biggest;
-            
         }
-
-
         return sumPriority;
     }
 
@@ -58,37 +58,35 @@ public class Solution {
 
         while((currentLine = br.readLine()) != null) {
             int mid = currentLine.length()/2;
+            int biggest = 0; //Highest priority in backpack
+            
             String s1 = currentLine.substring(0, mid);
             String s2 = currentLine.substring(mid);
-            int biggest = 0; //Highest priority in backpack
 
-            for(int i = 0; i < s1.length(); i++) {
-                
-                char c = s1.charAt(i);
-                int prio = Alphabet.getNum(c);
+            char[] c1 = s1.toCharArray(); 
+            char[] c2 = s2.toCharArray();
+           
 
-                for(int j = 0; j < s2.length(); j++) {
-                    if(c == s2.charAt(j)) {
-                        if(prio > biggest) {
-                            biggest = prio;
-                        }
-                    }
+            Set<Character> inString = new HashSet<>();
 
+            for(char c : c1) {
+                inString.add(c);
+            }
+            for(char c : c2) {
+                if(inString.contains(c)){
+                    biggest = Math.max(Alphabet.getNum(c), biggest);
                 }
-                
-             }
-             sumPriority += biggest;
-
+            }
+            sumPriority += biggest;
         }
         return sumPriority;
     }
 
     public enum Alphabet {
-        a, b, c, d, e, f, g, h,i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+        a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
 
         public static int getNum(char c) {
             return valueOf(String.valueOf(c)).ordinal() + 1;
         }
-
     }
 }
