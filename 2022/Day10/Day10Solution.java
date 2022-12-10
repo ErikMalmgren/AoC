99b6d985f845b6aa8aedc8ea7eb12465875b4a13
Erik Malmgren <er8540ma-s@student.lu.se>
2022-12-10 18:44:45 +0100

    Dag 10 äntligen ikapp

---
+++ 2022/Day10/Day10Solution.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Day10Solution {
+    private static Map<Integer, Integer> clockCycles; //Första int är klockcykel och andra int är x-värdet
+    
+    public static void main(String[] args) throws IOException{
+        calculate();
+    }
+
+    private static void calculate() throws IOException{
+        readFile();
+        sumSignalStrength();
+        drawCRT();
+        //System.out.println(clockCycles);
+    }
+
+    private static void readFile() throws IOException {
+        var input = new File("2022/inputs/day10.txt");
+        var reader = new BufferedReader(new FileReader(input));
+
+        clockCycles = new HashMap<>();
+        int x = 1;
+        int cycle = 1;
+        
+
+        String line;
+
+        while((line = reader.readLine()) != null) {
+            String[] parts = line.split(" ");
+
+            if(parts[0].equals("noop")) {
+                clockCycles.put(cycle, x);
+                cycle++;
+                continue;
+            } else {
+                clockCycles.put(cycle++, x);
+                clockCycles.put(cycle++, x);
+                x += Integer.parseInt(parts[1]);
+                clockCycles.put(cycle, x);
+            }
+
+
+        }
+    }
+
+    private static void sumSignalStrength() {
+        int sum = 0;
+        for (int i = 20; i < 221; i += 40) {
+            sum += clockCycles.get(i) * i;
+        }
+        System.out.println("Part one: " + sum);
+    }
+
+    private static void drawCRT() {
+        String screen = "";
+        int pixel = 0;
+        String sign = "";
+        for(int i = 1; i < clockCycles.size(); i++) {
+            int x = clockCycles.get(i);
+            if(pixel == x-1 || pixel == x || pixel == x+1) {
+                sign = "#";
+            } else {
+                sign = ".";
+            }
+            screen += sign;
+            pixel++;
+
+            if(pixel % 40 == 0) {
+                screen += "\n";
+                pixel = 0;
+            }
+        }
+        System.out.print(screen);
+    }
+}
